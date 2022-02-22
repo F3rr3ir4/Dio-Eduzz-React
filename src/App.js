aa1e4db3c624efe10a8bc5dbf16d6895265f86fc
F3rr31r4 <leandroferreira757@msn.com>
2022-02-21 21:41:41 -0300

    created component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import Item from "./components/Item";
+import Card from "./components/Card";
+
 const App = ( )=> {
-  return (
-    <h1>Hello Word!</h1>
+  return (<>
+    <h1>Minha primeira aplicação com React </h1>
+    <ul>
+    <Item texto="Item 1"/>
+    <Item texto="Item 2"/>
+    <Item texto="Item 3"/>
+      
+
+  </ul>
+  <Card></Card>
+    </>
 
   )
 }
